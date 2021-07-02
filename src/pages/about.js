7e1b18667312b77f8e1259c10a67e29afb78fa76
Max Scher <maxobaxo@gmail.com>
2021-07-02 15:29:27 -0700

    update blog details

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,23 @@ import Layout from '../components/Layout'
 const About = () => (
   <Layout>
     <h1>About</h1>
-    <p>Hi, I'm Max.</p>
-    <p></p>
+    <p>
+      Hi, I'm Max. I'm currently a full stack software engineer at Nike, where I
+      work in JavaScript and React.
+    </p>
+    <blockquote>
+      “Just because I choose not to drink doesn’t automatically make me no fun.
+      That is a separate choice, which I have also made.”{' '}
+      <a href='https://twitter.com/trisarahjtops'>-@trisarahjtops</a>
+    </blockquote>
+    <p>Some things about me:</p>
+    <ul>
+      <li>I'm a Dad and a Husband</li>
+      <li>
+        I'm allergic to a lot of foods: dairy, all nuts, poppy seeds, hemp.
+      </li>
+      <li>I used to work in the motion picture business.</li>
+    </ul>
   </Layout>
 )
 
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -21,12 +21,6 @@ const Blog = ({ data }) => (
         />
       )
     })}
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dignissimos
-      aut consequuntur aspernatur corrupti ratione, odit similique tenetur
-      accusantium, est nostrum esse minus iure voluptatum nihil cumque
-      blanditiis non? Odit.
-    </p>
   </Layout>
 )
 
